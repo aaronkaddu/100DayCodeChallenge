e05c376c9da89e214e3ddf22a107f3d7253567f4
aaronkaddu <aaronmuzira@gmail.com>
2025-01-24 12:46:37 +0300

    Added more code about type conversion

--- DAY 2/DataTypes.py
+++ DAY 2/DataTypes.py
@@ -13,3 +13,14 @@ print(3.4566 + 45.677)
 # Boolean
 print(True)
 print(False)
+
+# printing data types
+print(type("aaron"))
+print(type(1234))
+print(type(3.456))
+print(type(True))
+print(type(123_4567_56))
+
+# Error correction
+
+print("Number of letters in your name: " + str(len(input("Enter your name: "))))
