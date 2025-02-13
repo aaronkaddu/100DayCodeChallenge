0a993fa8670df681ce951e5d10deea8f37760e1f
aaronkaddu <aaronmuzira@gmail.com>
2025-02-13 14:00:26 +0300

    Ptyhon Basics

--- DAY 6/gameBegoori.py
+++ DAY 6/gameBegoori.py
@@ -1,15 +1,14 @@
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-def jump():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-
-
-for step in range(6):
-    jump()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#
+# for step in range(6):
+#     jump()
